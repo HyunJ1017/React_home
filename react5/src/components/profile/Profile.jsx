01b7507566f578f377ed6c544f268c450bf14ea0
DESKTOP-DNRTKFK\slev5 <hyunj1017@naver.com>
2025-02-08 14:43:03 +0900

    no message

--- react5/src/components/profile/Profile.jsx
+++ react5/src/components/profile/Profile.jsx
@@ -4,43 +4,43 @@ const Profile = () => {
   return (
     <>
       <section class="contents-section rowbox" id="profile-section">
-  <div class="harf-row">
-    <div class="img-container">
-      <img src="images/profile.jpg" id="profile-img" alt="내사진" />
-    </div>
-  </div>
-  <div class="harf-row" id="profile-box">
-    <h1>박현준</h1>
-    <h4>Park Hyunj Jun</h4>
-    <p id="email">
-      <i class="fa-duotone fa-regular fa-envelopes" style="--fa-primary-color: #3b9eec; --fa-secondary-color: #ffffff;"></i>
-      hyunj1017@naver.com
-    </p>
-    <p id="phone">
-      <i class="fa-duotone fa-light fa-phone" style="--fa-primary-color: #137fd9; --fa-secondary-color: #137fd9;"></i>
-      010-2113-3766
-    </p>
-    <p id="locate">
-      <i class="fa-sharp-duotone fa-solid fa-compass" style="--fa-primary-color: #ff0000; --fa-secondary-color: #ffffff;"></i>
-      인천 동구
-    </p>
-    <div class="skill-box" id="mainSkill-box">
-      <a class="link-on sliderA" href="#stack-html">HTML</a>
-      <a class="link-on sliderA" href="#stack-js">JS</a>
-      <div class="link-none">CSS</div>
-      <a class="link-on sliderA" href="#stack-oracle">Oracle</a>
-      <div class="link-none">WebSocket</div>
-      <div class="link-none">Redis</div>
-      <div class="link-none">React</div>
-      <div class="link-none">GitHub</div>
-      <a class="link-on sliderA" href="#stack-fireBase">FireBase</a>
-    </div>
-    <div id="link-icon">
-      <a href="https://github.com/HyunJ1017" target="_blank"><i class="fa-brands fa-github" style="color: #ffffff;"></i></a>
-      <a href="https://hyunj1017.tistory.com/" target="_blank"><i class="fa-solid fa-circle-t"></i></a>
-    </div>
-  </div>
-</section>
+        <div class="harf-row">
+          <div class="img-container">
+            <img src="images/profile.jpg" id="profile-img" alt="내사진" />
+          </div>
+        </div>
+        <div class="harf-row" id="profile-box">
+          <h1>박현준</h1>
+          <h4>Park Hyunj Jun</h4>
+          <p id="email">
+            <i class="fa-duotone fa-regular fa-envelopes" style="--fa-primary-color: #3b9eec; --fa-secondary-color: #ffffff;"></i>
+            hyunj1017@naver.com
+          </p>
+          <p id="phone">
+            <i class="fa-duotone fa-light fa-phone" style="--fa-primary-color: #137fd9; --fa-secondary-color: #137fd9;"></i>
+            010-2113-3766
+          </p>
+          <p id="locate">
+            <i class="fa-sharp-duotone fa-solid fa-compass" style="--fa-primary-color: #ff0000; --fa-secondary-color: #ffffff;"></i>
+            인천 동구
+          </p>
+          <div class="skill-box" id="mainSkill-box">
+            <a class="link-on sliderA" href="#stack-html">HTML</a>
+            <a class="link-on sliderA" href="#stack-js">JS</a>
+            <div class="link-none">CSS</div>
+            <a class="link-on sliderA" href="#stack-oracle">Oracle</a>
+            <div class="link-none">WebSocket</div>
+            <div class="link-none">Redis</div>
+            <div class="link-none">React</div>
+            <div class="link-none">GitHub</div>
+            <a class="link-on sliderA" href="#stack-fireBase">FireBase</a>
+          </div>
+          <div id="link-icon">
+            <a href="https://github.com/HyunJ1017" target="_blank"><i class="fa-brands fa-github" style="color: #ffffff;"></i></a>
+            <a href="https://hyunj1017.tistory.com/" target="_blank"><i class="fa-solid fa-circle-t"></i></a>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
