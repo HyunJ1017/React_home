1038c270c676dac9fd47b8a6e58137cda2aa8d43
DESKTOP-DNRTKFK\slev5 <hyunj1017@naver.com>
2025-03-18 15:18:48 +0900

    no message

--- react5/src/components/career/Career.jsx
+++ react5/src/components/career/Career.jsx
@@ -11,7 +11,95 @@ const Career = () => {
             <img src="images/logo.jpg" id="profile-img" alt="내사진">
           </div>
         </div>
-        
+        <div class="harf-row" id="log-contents">
+          <ul>
+            <li>훈련기관</li>
+            <li>KH정보교육원</li>
+            <li>훈련과정</li>
+            <li>공공데이터 융합 자바개발자 양성과정</li>
+            <li>훈련직종</li>
+            <li>디지털컨버전스</li>
+            <li>훈련기간</li>
+            <li>2024.06.24 - 2024.12.06</li>
+            <li><i class="fa-solid fa-share-from-square" style="color: #ffffff;"></i>세미 프로젝트 링크(준비중)</li>
+            <li></li>
+            <li><a target="_blank" href="http://3.104.109.149/main"><i class="fa-solid fa-share-from-square" style="color: #ffffff;"></i>파이널 프로젝트 링크</a></li>
+          </ul>
+          <div class="skill-box" id="logSkill-box">
+            <div class="skill-relative" >
+              <div class="skill-change">UI 디자인</div>
+              <div class="skill-absolute">100/100</div>
+            </div>
+            <div class="skill-relative" >
+              <div class="skill-change">UI 구현</div>
+              <div class="skill-absolute">100/100</div>
+            </div>
+            <div class="skill-relative" >
+              <div class="skill-change">프로그래밍 언어 활용</div>
+              <div class="skill-absolute">100/100</div>
+            </div>
+            <div class="skill-relative" >
+              <div class="skill-change">프로그래밍 언어 응용</div>
+              <div class="skill-absolute">100/100</div>
+            </div>
+            <div class="skill-relative" >
+              <div class="skill-change">네트워크 프로그래밍 구현</div>
+              <div class="skill-absolute">100/100</div>
+            </div>
+            <div class="skill-relative" >
+              <div class="skill-change">SQL활용</div>
+              <div class="skill-absolute">100/100</div>
+            </div>
+            <div class="skill-relative" >
+              <div class="skill-change">데이터베이스 구현</div>
+              <div class="skill-absolute">100/100</div>
+            </div>
+            <div class="skill-relative" >
+              <div class="skill-change">SQL응용</div>
+              <div class="skill-absolute">75/100</div>
+            </div>
+            <div class="skill-relative" >
+              <div class="skill-change">인터페이스 구현</div>
+              <div class="skill-absolute">100/100</div>
+            </div>
+            <div class="skill-relative" >
+              <div class="skill-change">서버프로그램 구현</div>
+              <div class="skill-absolute">87.5/100</div>
+            </div>
+            <div class="skill-relative" >
+              <div class="skill-change">통합 구현</div>
+              <div class="skill-absolute">100/100</div>
+            </div>
+            <div class="skill-relative" >
+              <div class="skill-change">요구사항 확인</div>
+              <div class="skill-absolute">100/100</div>
+            </div>
+            <div class="skill-relative" >
+              <div class="skill-change">애플리케이션 설계</div>
+              <div class="skill-absolute">80/100</div>
+            </div>
+            <div class="skill-relative" >
+              <div class="skill-change">프로젝트 기반 공공데이터 아키텍처 설계</div>
+              <div class="skill-absolute">100/100</div>
+            </div>
+            <div class="skill-relative" >
+              <div class="skill-change">프로젝트 기반 공공데이터 활용</div>
+              <div class="skill-absolute">100/100</div>
+            </div>
+            <div class="skill-relative" >
+              <div class="skill-change">화면 구현</div>
+              <div class="skill-absolute">100/100</div>
+            </div>
+            <div class="skill-relative" >
+              <div class="skill-change">애플리케이션 배포</div>
+              <div class="skill-absolute">100/100</div>
+            </div>
+            <div class="skill-relative" >
+              <div class="skill-change">애플리케이션 테스트 수행</div>
+              <div class="skill-absolute">100/100</div>
+            </div>
+          </div>
+        </div>
       </section>
     </>
   );
