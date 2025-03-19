ddea0004f042647fbdb26f55a1c10b443b6d67a8
DESKTOP-DNRTKFK\slev5 <hyunj1017@naver.com>
2025-03-19 14:34:32 +0900

    no message

--- react5/src/components/career/Career.jsx
+++ react5/src/components/career/Career.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 
+import ProfileImg from 'images/circle_cropped_cat.png';
+
 const Career = () => {
   return (
     <>
-      <h1>Career</h1>
+      <section className="contents-section rowbox" id="log-section">
 
-      <section class="contents-section rowbox" id="log-section">
-        <div class="harf-row">
-          <div class="img-container">
-            <img src="images/logo.jpg" id="profile-img" alt="내사진">
+        <div className="harf-row">
+          <div className="img-container">
+            <img src={ProfileImg} id="profile-img" alt="내사진" />
           </div>
         </div>
-        <div class="harf-row" id="log-contents">
+
+        <div className="harf-row" id="log-contents">
           <ul>
             <li>훈련기관</li>
             <li>KH정보교육원</li>
@@ -21,86 +23,88 @@ const Career = () => {
             <li>디지털컨버전스</li>
             <li>훈련기간</li>
             <li>2024.06.24 - 2024.12.06</li>
-            <li><i class="fa-solid fa-share-from-square" style="color: #ffffff;"></i>세미 프로젝트 링크(준비중)</li>
+            <li><i className="fa-solid fa-share-from-square"></i>세미 프로젝트 링크(준비중)</li>
             <li></li>
-            <li><a target="_blank" href="http://3.104.109.149/main"><i class="fa-solid fa-share-from-square" style="color: #ffffff;"></i>파이널 프로젝트 링크</a></li>
+            <li><a target="_blank" href="http://3.104.109.149/main"><i className="fa-solid fa-share-from-square" ></i>파이널 프로젝트 링크</a></li>
           </ul>
-          <div class="skill-box" id="logSkill-box">
-            <div class="skill-relative" >
-              <div class="skill-change">UI 디자인</div>
-              <div class="skill-absolute">100/100</div>
+          <div className="skill-box" id="logSkill-box">
+            <div className="skill-relative" >
+              <div className="skill-change">UI 디자인</div>
+              <div className="skill-absolute">100/100</div>
             </div>
-            <div class="skill-relative" >
-              <div class="skill-change">UI 구현</div>
-              <div class="skill-absolute">100/100</div>
+            <div className="skill-relative" >
+              <div className="skill-change">UI 구현</div>
+              <div className="skill-absolute">100/100</div>
             </div>
-            <div class="skill-relative" >
-              <div class="skill-change">프로그래밍 언어 활용</div>
-              <div class="skill-absolute">100/100</div>
+            <div className="skill-relative" >
+              <div className="skill-change">프로그래밍 언어 활용</div>
+              <div className="skill-absolute">100/100</div>
             </div>
-            <div class="skill-relative" >
-              <div class="skill-change">프로그래밍 언어 응용</div>
-              <div class="skill-absolute">100/100</div>
+            <div className="skill-relative" >
+              <div className="skill-change">프로그래밍 언어 응용</div>
+              <div className="skill-absolute">100/100</div>
             </div>
-            <div class="skill-relative" >
-              <div class="skill-change">네트워크 프로그래밍 구현</div>
-              <div class="skill-absolute">100/100</div>
+            <div className="skill-relative" >
+              <div className="skill-change">네트워크 프로그래밍 구현</div>
+              <div className="skill-absolute">100/100</div>
             </div>
-            <div class="skill-relative" >
-              <div class="skill-change">SQL활용</div>
-              <div class="skill-absolute">100/100</div>
+            <div className="skill-relative" >
+              <div className="skill-change">SQL활용</div>
+              <div className="skill-absolute">100/100</div>
             </div>
-            <div class="skill-relative" >
-              <div class="skill-change">데이터베이스 구현</div>
-              <div class="skill-absolute">100/100</div>
+            <div className="skill-relative" >
+              <div className="skill-change">데이터베이스 구현</div>
+              <div className="skill-absolute">100/100</div>
             </div>
-            <div class="skill-relative" >
-              <div class="skill-change">SQL응용</div>
-              <div class="skill-absolute">75/100</div>
+            <div className="skill-relative" >
+              <div className="skill-change">SQL응용</div>
+              <div className="skill-absolute">75/100</div>
             </div>
-            <div class="skill-relative" >
-              <div class="skill-change">인터페이스 구현</div>
-              <div class="skill-absolute">100/100</div>
+            <div className="skill-relative" >
+              <div className="skill-change">인터페이스 구현</div>
+              <div className="skill-absolute">100/100</div>
             </div>
-            <div class="skill-relative" >
-              <div class="skill-change">서버프로그램 구현</div>
-              <div class="skill-absolute">87.5/100</div>
+            <div className="skill-relative" >
+              <div className="skill-change">서버프로그램 구현</div>
+              <div className="skill-absolute">87.5/100</div>
             </div>
-            <div class="skill-relative" >
-              <div class="skill-change">통합 구현</div>
-              <div class="skill-absolute">100/100</div>
+            <div className="skill-relative" >
+              <div className="skill-change">통합 구현</div>
+              <div className="skill-absolute">100/100</div>
             </div>
-            <div class="skill-relative" >
-              <div class="skill-change">요구사항 확인</div>
-              <div class="skill-absolute">100/100</div>
+            <div className="skill-relative" >
+              <div className="skill-change">요구사항 확인</div>
+              <div className="skill-absolute">100/100</div>
             </div>
-            <div class="skill-relative" >
-              <div class="skill-change">애플리케이션 설계</div>
-              <div class="skill-absolute">80/100</div>
+            <div className="skill-relative" >
+              <div className="skill-change">애플리케이션 설계</div>
+              <div className="skill-absolute">80/100</div>
             </div>
-            <div class="skill-relative" >
-              <div class="skill-change">프로젝트 기반 공공데이터 아키텍처 설계</div>
-              <div class="skill-absolute">100/100</div>
+            <div className="skill-relative" >
+              <div className="skill-change">프로젝트 기반 공공데이터 아키텍처 설계</div>
+              <div className="skill-absolute">100/100</div>
             </div>
-            <div class="skill-relative" >
-              <div class="skill-change">프로젝트 기반 공공데이터 활용</div>
-              <div class="skill-absolute">100/100</div>
+            <div className="skill-relative" >
+              <div className="skill-change">프로젝트 기반 공공데이터 활용</div>
+              <div className="skill-absolute">100/100</div>
             </div>
-            <div class="skill-relative" >
-              <div class="skill-change">화면 구현</div>
-              <div class="skill-absolute">100/100</div>
+            <div className="skill-relative" >
+              <div className="skill-change">화면 구현</div>
+              <div className="skill-absolute">100/100</div>
             </div>
-            <div class="skill-relative" >
-              <div class="skill-change">애플리케이션 배포</div>
-              <div class="skill-absolute">100/100</div>
+            <div className="skill-relative" >
+              <div className="skill-change">애플리케이션 배포</div>
+              <div className="skill-absolute">100/100</div>
             </div>
-            <div class="skill-relative" >
-              <div class="skill-change">애플리케이션 테스트 수행</div>
-              <div class="skill-absolute">100/100</div>
+            <div className="skill-relative" >
+              <div className="skill-change">애플리케이션 테스트 수행</div>
+              <div className="skill-absolute">100/100</div>
             </div>
           </div>
         </div>
+
       </section>
+
     </>
   );
 };
--- react5/src/components/profile/Profile.jsx
+++ react5/src/components/profile/Profile.jsx
@@ -18,15 +18,15 @@ const Profile = () => {
           <h1>박현준</h1>
           <h4>Park Hyunj Jun</h4>
           <p id="email">
-            <i className="fa-duotone fa-regular fa-envelopes" style="--fa-primary-color: #3b9eec; --fa-secondary-color: #ffffff;"></i>
+            <i className="fa-duotone fa-regular fa-envelopes"></i>
             hyunj1017@naver.com
           </p>
           <p id="phone">
-            <i className="fa-duotone fa-light fa-phone" style="--fa-primary-color: #137fd9; --fa-secondary-color: #137fd9;"></i>
+            <i className="fa-duotone fa-light fa-phone"></i>
             010- 2113-3766
           </p>
           <p id="locate">
-            <i className="fa-sharp-duotone fa-solid fa-compass" style="--fa-primary-color: #ff0000; --fa-secondary-color: #ffffff;"></i>
+            <i className="fa-sharp-duotone fa-solid fa-compass"></i>
             인천 동구
           </p>
           <div className="skill-box" id="mainSkill-box">
@@ -41,7 +41,7 @@ const Profile = () => {
             <a className="link-on sliderA" href="#stack-fireBase">FireBase</a>
           </div>
           <div id="link-icon">
-            <a href="https://github.com/HyunJ1017" target="_blank"><i className="fa-brands fa-github" style="color: #ffffff;"></i></a>
+            <a href="https://github.com/HyunJ1017" target="_blank"><i className="fa-brands fa-github" ></i></a>
             <a href="https://hyunj1017.tistory.com/" target="_blank"><i className="fa-solid fa-circle-t"></i></a>
           </div>
 
